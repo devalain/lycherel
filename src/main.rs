9902360e65d1bece6efe8c4cef30957665e5e4bf
Alain Buisseret <me@alain.dev>
2021-10-10 21:24:59 +0200

    Premier nombre de Lycherel en octal: 0o1775

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::cmp::{PartialEq, PartialOrd};
-use std::fmt::{Debug, Display, UpperHex};
+use std::fmt::{Debug, Display};
 use std::ops::{AddAssign, DivAssign, MulAssign, Rem};
 
 use rug::Integer;
@@ -43,10 +43,10 @@ fn mirror<T: Num>(num: &T, base: &T) -> T {
 fn is_palindrome<T: Num + PartialEq>(num: &T, base: &T) -> bool {
     num == &mirror(num, base)
 }
-fn lycherel<T: Num + UpperHex>(num: T, base: T) {
+fn lycherel<T: Num + std::fmt::Octal>(num: T, base: T) {
     let mut l = num;
     loop {
-        // println!("{:X}", l);
+        //println!("{:o}", l);
         if is_palindrome(&l, &base) {
             break;
         }
@@ -55,14 +55,16 @@ fn lycherel<T: Num + UpperHex>(num: T, base: T) {
 }
 
 // Lychrel numbers found
+// base-2 : 10110
+// base-8 : 1775
 // base-10: 196
 // base-16: 19D
-// base-2 : 10110
 fn main() {
     let mut i = Integer::from(1);
-    let base = Integer::from(2);
+    let base = Integer::from(8);
     loop {
-        println!("{:b}", i);
+        println!("\n\n{:o}\n", i);
+        std::thread::sleep(std::time::Duration::from_millis(2000));
         lycherel(i.clone(), base.clone());
         i += 1;
     }
